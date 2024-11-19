23967a7208980b2ddba7fbcdc13dad9410b8b56b
Ian Page Maciel <ianpage27@gmail.com>
2024-11-19 17:14:10 -0300

    Exercicios

---
+++ exercicio09.js
@@ -0,0 +1,10 @@
+var velo = 80.5
+var max = 90
+console.log(`a velocidade do carro é ${velo} KM/H`)//colocando crase eu interrligo os assuntos 
+
+
+if (velo > max) { //condição simples se tiver somente o if
+    console.log('vc pasou da velocidade maxima')
+}
+
+console.log ('use cinto de segunraça')
---
+++ exercicio10.js
@@ -0,0 +1,8 @@
+var pais = 'Estados Unidos'
+console.log(`Eu sou de tal pais ${pais}`)
+if (pais == 'Brasil') { //condição simples se tiver somente o if
+    console.log('Brasileiro')
+}
+else{
+    console.log('Estranjeiro')
+}
